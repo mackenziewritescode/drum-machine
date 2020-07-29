542d65afbcad467bf0895f82dd38b86a1f505a3f
mackenziewritescode <mackenzie.charlton@gmail.com>
2020-07-29 18:05:39 -0400

    building the layout

--- src/App.js
+++ src/App.js
@@ -1,42 +1,63 @@
-import React from "react"
-import "./App.css"
+import React, { Component } from "react";
+import "./App.css";
 
-function App() {
-  return (
-    <div id="wrapper">
-      <div id="drum-machine">
-        <div id="pad-wrap">
-          <div className="pad" id="pad-1">
-            Q
-          </div>
-          <div className="pad" id="pad-2">
-            W
-          </div>
-          <div className="pad" id="pad-3">
-            E
-          </div>
-          <div className="pad" id="pad-4">
-            A
-          </div>
-          <div className="pad" id="pad-5">
-            S
-          </div>
-          <div className="pad" id="pad-6">
-            D
-          </div>
-          <div className="pad" id="pad-7">
-            Z
-          </div>
-          <div className="pad" id="pad-8">
-            X
-          </div>
-          <div className="pad" id="pad-9">
-            C
+class App extends Component {
+  constructor() {
+    super();
+    this.state = {};
+    this.handlePad = this.handlePad.bind(this);
+  }
+
+  handlePad(event) {
+    console.log(event.target.name);
+  }
+
+  render() {
+    return (
+      <div id="wrapper">
+        <div id="drum-machine">
+          <div id="pad-wrap">
+            <div className="pad" name="pad-1" onMouseDown={this.handlePad}>
+              <p className="letter">Q</p>
+              <p>clap</p>
+            </div>
+            <div className="pad" id="pad-2">
+              <p className="letter">W</p>
+              <p>clap</p>
+            </div>
+            <div className="pad" id="pad-3">
+              <p className="letter">E</p>
+              <p>clap</p>
+            </div>
+            <div className="pad" id="pad-4">
+              <p className="letter">A</p>
+              <p>clap</p>
+            </div>
+            <div className="pad" id="pad-5">
+              <p className="letter">S</p>
+              <p>clap</p>
+            </div>
+            <div className="pad" id="pad-6">
+              <p className="letter">D</p>
+              <p>clap</p>
+            </div>
+            <div className="pad" id="pad-7">
+              <p className="letter">Z</p>
+              <p>clap</p>
+            </div>
+            <div className="pad" id="pad-8">
+              <p className="letter">X</p>
+              <p>clap</p>
+            </div>
+            <div className="pad" id="pad-9">
+              <p className="letter">C</p>
+              <p>clap</p>
+            </div>
           </div>
         </div>
       </div>
-    </div>
-  )
+    );
+  }
 }
 
-export default App
+export default App;
